2384915e872984761c93d3f95438a696cbfa227e
Pawarisron Wittaya <pawarisron.w@gmail.com>
2025-07-09 11:51:50 +0700

    add api filter transaction

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,20 +1,100 @@
+import { Category } from './../entities/Category';
 import { Transaction } from './../entities/Transaction';
 import { createTransactionByUserIdSchema } from './../schemas/transactions/createTransactionByUserIdSchema';
 import { FastifyReply } from "fastify"
 import { UserPrincipleRequest } from "../types/UserPrincipleRequest"
 import { AppDataSource } from '../database/dataSource';
 import { uploadTransactionSlipByIdSchema } from '../schemas/transactions/uploadTransactionSlipByIdSchema';
+import { getAllTransactionByUserIdSchema } from '../schemas/transactions/getAllTransactionByUserIdSchema';
 import path from 'path';
 import fs from 'fs';
-const leoProfanity = require('leo-profanity');
+import { config } from '../../config';
+const leoProfanity = require('leo-profanity'); //bad word filter
 
 //Get all transaction filter
 export const getAllTransactionByUserId = async (req:UserPrincipleRequest, reply:FastifyReply) =>{
+    try{
+        const {error, value} = getAllTransactionByUserIdSchema.validate(req.query || null)
+        if(error){
+            return reply.code(400).send({ message: 'Invalid query parameters', details: error.details });
+        }
+        console.log(value)
+        const userId = req.user?.id;
+        if(!userId){
+            return reply.code(401).send({ message: 'Unauthorized'});
+        }
+        //repo joined table
+        const query = AppDataSource
+        .getRepository(Transaction)
+        .createQueryBuilder('t')
+        .leftJoinAndSelect('t.account', 'account')
+        .leftJoinAndSelect('t.category', 'category')
+        .where('t.user_id = :userId', { userId }); // only transaction that belong to this userId
+
+        const month = value.month;
+        const year = value.year;
+        const type = value.type;
+        const accountId = value.accountId;
+        const categoryId = value.categoryId;
+
+        //Filter month && year
+        if (month && year) {
+            query.andWhere('EXTRACT( MONTH FROM t.transaction_date ) = :month', { month });
+            query.andWhere('EXTRACT( YEAR FROM t.transaction_date ) = :year', { year });
+        }
+        //Filter Type
+        if(type){
+            query.andWhere('category.type = :type', {type} );
+        }
+        //Filter AccountId
+        if(accountId){
+            query.andWhere('t.account.id = :accountId', {accountId} );
+        }
+        //Filter CategoryId
+        if(categoryId){
+            query.andWhere('t.category_id = :categoryId', { categoryId });
+        }
+
+        // Pagination
+        const page = value.page || 1;
+        const size = config.allowedPageSize.includes(value.size) ? value.size : config.allowedPageSize[0];
+        const skip = (page  - 1) * size;
+
+        const [transactions, total] = await query
+        .orderBy('t.transaction_date', 'DESC')
+        .skip(skip)
+        .take(size)
+        .select([
+            't.id',
+            't.user_id',
+            't.amount',
+            't.transaction_date',
+            'category.name',
+            'account.name',
+        ])
+        .getManyAndCount();
+        return {
+            data: transactions,
+            pagination: {
+                total,
+                page,
+                size: size,
+                totalPages: Math.ceil(total / size),
+            }
+        };
+
+    }catch(error){
+        req.log.error(error);
+        reply.code(500).send({ message: 'Internal Server Error', details: error });
+    }
+
+
 
 }
 //Get Single transaction
 export const getTransactionById = async (req:UserPrincipleRequest, reply:FastifyReply) =>{
-
+    
+    
 }
 //Create Transaction
 export const createTransactionByUserId = async (req:UserPrincipleRequest, reply:FastifyReply) =>{
---
+++ src/schemas/transactions/getAllTransactionByUserIdSchema.ts
@@ -0,0 +1,10 @@
+import Joi from "joi";
+import { paginationSchema } from "../paginationSchema";
+
+export const getAllTransactionByUserIdSchema = paginationSchema.keys({
+    month:Joi.number().integer().min(1).max(12),
+    year:Joi.number().integer().positive(),
+    type: Joi.string().valid('INCOME', 'EXPENSE'),
+    accountId: Joi.string().uuid(),
+    categoryId: Joi.string().uuid(),
+})
